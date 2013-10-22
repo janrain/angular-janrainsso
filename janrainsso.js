df48a323f6600a97a6b8ab74c764be0680503f1d
Joe Moon <joe@xoxomoon.com>
2013-10-21 18:17:22 -0700

    Removed dependency on static sso.html page

--- janrainsso.js
+++ janrainsso.js
@@ -1,7 +1,7 @@
 'use strict';
 var app = angular.module('janrainSso', ['ngCookies', 'janrainErrors']);
 
-app.factory('DashboardAuth', function($location, $cookies, $http, $window, AUTH_URI, ROOT_URL, SSO_URL, UD_URL, janrainErrorsSvc) {
+app.factory('DashboardAuth', function($location, $cookies, $http, $window, $route, AUTH_URI, ROOT_URL, SSO_URL, UD_URL, CLIENT_ID, janrainErrorsSvc) {
 
   var params = getQueryParams();
 
@@ -28,7 +28,29 @@ app.factory('DashboardAuth', function($location, $cookies, $http, $window, AUTH_
       if (data.status === 401) {
 
         $cookies.originalRequest = $location.path();
-        $window.location.href = ROOT_URL + 'sso.html';
+        $window.janrain = {
+          capture: {
+            ui: {
+              UNIDASH_SSO_NOLOGIN_HANDLER: function(data) {
+                $window.location = UD_URL
+                                 + '/signin?dest='
+                                 + encodeURIComponent($window.location.href)
+                                 ;
+              }
+            }
+          }
+        };
+
+        JANRAIN.SSO.CAPTURE.check_login({
+          sso_server: SSO_URL
+        , client_id: CLIENT_ID
+        , redirect_uri: $window.location.origin + ROOT_URL
+        , xd_receiver: ''
+        , logout_uri: ''
+        , nologin_callback: 'UNIDASH_SSO_NOLOGIN_HANDLER'
+        , refresh: true
+        });
+
         return { authenticated: false };
 
       } else {
@@ -45,27 +67,36 @@ app.factory('DashboardAuth', function($location, $cookies, $http, $window, AUTH_
 		return $http.delete(AUTH_URI)
 		.then(function(){
 			$window.JANRAIN.SSO.CAPTURE.logout({
-	      sso_server: SSO_URL,
-	      logout_uri: UD_URL,
+	      sso_server: SSO_URL
+	    , logout_uri: UD_URL
 	   	});
 		});
 	};
 
   function createSession(token, origin) {
-    var redirect_uri = $window.location.protocol + '//' + $window.location.host + ROOT_URL + '?origin=' + encodeURIComponent(origin);
+    var redirect_uri = $window.location.protocol
+                     + '//'
+                     + $window.location.host
+                     + ROOT_URL
+                     + '?origin='
+                     + encodeURIComponent(origin)
+                     ;
+
     return $http.post(AUTH_URI, null, {
       params: {
-        'code': token,
-        'redirect_uri': redirect_uri
+        'code': token
+      , 'redirect_uri': redirect_uri
       }
     })
     .then(function(res) {
 
       /* TODO: this is a hard redirect (page reload)
        * because the query string in the url is before
-       * the hash (#). Need to change the redirect url
-       * from sso.html to /#/
+       * the hash (#). The sso widget appears to be
+       * rewriting the the url it redirects the browser
+       * to to put the query string before the hash.
        */
+
       $window.location.href = ROOT_URL;
 
       return makeAuthObj(res.data);
@@ -94,6 +125,11 @@ app.factory('DashboardAuth', function($location, $cookies, $http, $window, AUTH_
   };
 
   function makeAuthObj(data) {
+
+    // TODO: normalize get and post return data
+    if (!data.user) {
+      data.user = data;
+    }
 
     data.authenticated = true;
     data.logout = logout;
